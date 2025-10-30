900988ebe1ba5bf0c987fd59656e3252f53a53a5
Titiss <Mathis.fcs.lav@gmail.com>
2025-10-30 21:57:38 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,1 @@
-# Ici se trouvera les informations de connection à la base de données MySQL.
+# Ici se trouve les informations de connection à la base de données MySQL mais pour une raison evidente elles ne seront pas données.
